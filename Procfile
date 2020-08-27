6c9229283c1962fa33ba7ff45e8c6861b8e949fc
ijones16 <jones58ian@gmail.com>
2020-08-27 11:52:06 -0400

    add todo

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/reagent-todo.jar clojure.main -m reagent-todo.server
---
+++ env/dev/cljs/reagent_todo/dev.cljs
@@ -0,0 +1,15 @@
+(ns ^:figwheel-no-load reagent-todo.dev
+  (:require
+    [reagent-todo.core :as core]
+    [devtools.core :as devtools]))
+
+(extend-protocol IPrintWithWriter
+  js/Symbol
+  (-pr-writer [sym writer _]
+    (-write writer (str "\"" (.toString sym) "\""))))
+
+(devtools/install!)
+
+(enable-console-print!)
+
+(core/init!)
